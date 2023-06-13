e5e5b4fc2036729325b4461b11ed742bb63bf01e
marciojb <marciorobertodasilava@gmail.com>
2023-06-13 18:50:32 -0300

    teste5

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,6 @@ import Aromaterapia from './routes/Aromaterapia/Aromaterapia';
 import SobreNos from './routes/SobreNos/SobreNos';
 import Login from './routes/Login/Login';
 import Cadastro from './routes/Cadastro/Cadastro';
-import Home from './routes/Home/Home';
 
 const router = createBrowserRouter([
   {
@@ -54,10 +53,6 @@ const router = createBrowserRouter([
   {
     path:"/auroraorganic/aromaterapia",
     element:<Aromaterapia/>  
-  },
-  {
-    path:"/auroraorganic/Home",
-    element:<Home/>  
   },
   {
     path:"/auroraorganic",

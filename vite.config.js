b0ef856c15de5abf53ab38c8aeb1373cc822a172
marciojb <marciorobertodasilava@gmail.com>
2023-06-14 16:22:05 -0300

    teste200

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/auroraorganic",
+  server: {
+    host: '0.0.0.0',
+    port: 80,
+  },
   plugins: [react()],
 })
